1e6ff0640c8b3446dcfdc08e8508805a99204775
jkalasas <alasasjohnkyle@gmail.com>
2022-12-20 09:45:48 +0800

    Proper weighted graph implementation

--- surveyguru/survey.py
+++ surveyguru/survey.py
@@ -18,13 +18,9 @@ class EffectType(IntEnum):
 
 
 class Connection:
-    def __init__(
-        self, q1: int, qs: Union(List[int], int), effect: EffectType, weight: float
-    ) -> None:
-        self.affector = q1
-        self.affected = type(qs) == list and qs or [qs]
-        self.effect = effect
+    def __init__(self, weight: float, effect: EffectType) -> None:
         self.weight = weight
+        self.effect = effect
 
 
 class Option:
@@ -40,15 +36,20 @@ class Option:
 
 
 class Question:
-    def __init__(self, question: str, options: List[Option]):
-        self.question = question
-        self.options = options
+    def __init__(self, id: int, qs: str, opts: List[Option]):
+        self.id = id
+        self.question = qs
+        self.options = opts
+        self.connection = {}
 
     def __repr__(self) -> str:
         return f"Question: {self.question}"
 
     def __str__(self) -> str:
         return f"Question: {self.question}"
+
+    def add_connection(self, qs_id: int, con: Connection):
+        self.connection[qs_id] = con
 
     def add_option(self, option: Option):
         self.options.append(option)
@@ -59,13 +60,13 @@ class Survey:
         self,
         title: str = "",
         description: str = "",
-        questions: Dict[int, Question] = {},
-        connections: List[Connection] = [],
+        qs: Dict[int, Question] = {},
+        no_qs: int = 0,
     ):
         self.title = title
         self.description = description
-        self.questions = questions
-        self.connections = connections
+        self.questions = qs
+        self.num_questions = no_qs
 
     def __repr__(self) -> str:
         return f"Survey: {self.title}"
@@ -77,30 +78,50 @@ class Survey:
         if q not in self.questions:
             raise ValueError(f"Question {q} does not exist")
 
+    def add_question(self, qs: Question):
+        if qs.id in self.questions:
+            raise ValueError(f"Question {qs.id} already in survey")
+        self.num_questions += 1
+        self.questions[qs.id] = qs
+
+    def get_question(self, id: int) -> Union(int, None):
+        return self.qs.get(id)
+
+    def add_connection(self, frm: int, to: int, con: Connection):
+        frm_qs = self.questions.get(frm, None)
+        to_qs = self.questions.get(to, None)
+
+        if frm_qs is None:
+            raise ValueError(f"Question {frm} not found")
+        if to_qs is None:
+            raise ValueError(f"Question {to} not found")
+
+        frm_qs.add_connection(to, con)
+        to_qs.add_connection(frm, con)
+
     @staticmethod
-    def from_dict(dict: dict) -> Survey:
+    def from_dict(data: dict) -> Survey:
         survey = Survey()
-        for key, value in dict.items():
+        for key, value in data.items():
             if key == "title":
                 survey.title = value
             elif key == "description":
                 survey.description = value
             elif key == "questions":
-                for question in value:
-                    id = question["id"]
-                    options = [
-                        Option(o["name"], o["value"]) for o in question["options"]
-                    ]
-                    survey.questions[id] = Question(question["question"], options)
+                for qs_data in value:
+                    qs = Question(
+                        qs_data["id"],
+                        qs_data["question"],
+                        [
+                            Option(opt["name"], opt["value"])
+                            for opt in qs_data["options"]
+                        ],
+                    )
+                    survey.add_question(qs)
             elif key == "connections":
-                for connection in value:
-                    if survey.check_q_exists(connection["affector"]):
-                        survey.connections.append(
-                            Connection(
-                                connection["affector"],
-                                connection["affected"],
-                                connection["effect"],
-                                connection["weight"],
-                            )
-                        )
+                for con_data in value:
+                    con = Connection(con_data["weight"], con_data["effect"])
+                    survey.add_connection(
+                        con_data["affector"], con_data["affected"], con
+                    )
         return survey
